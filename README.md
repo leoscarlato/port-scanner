4f3aca9746fbdd5cf8bee8373a4edccb02df6998
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-08-25 19:30:30 -0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# port-scanner
+# Port Scanner
+
+Projeto para a matéria de Cybersegurança do curso de Ciência da Computação.
+
+## Objetivo
+Desenvolvimento de uma aplicação que realize o escaneamento de portas
+de comunicação de um destino por meio de bibliotecas de desenvolvimento da
+Linguagem de programação Python.
+
+Além disso, a aplicação deveria possuír uma interface amigável para o usuário e de fácil utilização.
+
+## Tecnologias utilizadas
+Foi utilizada a linguagem Python, junto à biblioteca Socket para realizar a verificação das portas de comunicação, além da biblioteca Tkinter para a criação da interface gráfica.
+
+## Funcionamento
+O usuário deve inserir o IP do destino e o intervalo de portas que deseja verificar. Após isso, basta clicar no botão "Começar verificação" e aguardar o resultado.
---
+++ main.py
@@ -0,0 +1,96 @@
+import socket
+import tkinter as tk
+from tkinter import messagebox
+
+# Project: Port Scanner
+# Details in README.md
+
+def scan_port(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.1)
+        result = s.connect_ex((host, port))
+        s.close()
+        return result == 0
+    except Exception as e:
+        return_message = f"Erro ao verificar a porta {port}: {str(e)}\n"
+        text_result.insert(tk.END, return_message)
+        window.update_idletasks()
+        return False
+    
+well_known_ports = {
+    20: "FTP",
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    993: "IMAP",
+    995: "POP3 ",
+    3306: "MySQL",
+    8080: "HTTP",
+    8443: "HTTPS"
+}
+
+def scan_range():
+    host = entry_host.get()
+    try:
+        start_port = int(entry_start_port.get())
+        end_port = int(entry_end_port.get())
+        if start_port < 0 or end_port < 0 or start_port > 65535 or end_port > 65535 or start_port > end_port:
+            messagebox.showerror("Erro", "Insira um valor válido para a porta (0-65535), com a porta de início sendo menor que a porta final.")
+            return
+    except ValueError:
+        messagebox.showerror("Erro", "Insira valores inteiros válidos.")
+        return
+
+    text_result.delete("1.0", tk.END)
+    status_label.config(text="Verificando portas, aguarde...") 
+
+    for port in range(start_port, end_port + 1):
+        if scan_port(host, port):
+            service = well_known_ports.get(port, "Unknown")
+            result_message = f"Porta {port} está aberta - {service}.\n"
+        else:
+            result_message = f"Porta {port} está fechada\n"
+        text_result.insert(tk.END, result_message)
+        window.update_idletasks()
+    text_result.insert(tk.END, "Verificação finalizada\n")
+    status_label.config(text="")  
+
+window = tk.Tk()
+window.title("Port Scanner")
+window.geometry("1280x720")
+
+label_title = tk.Label(window, text="Port Scanner", font=("Arial Bold", 30))
+label_title.pack()
+
+label_host = tk.Label(window, text="Host:", font=("Arial", 12))
+label_host.pack()
+entry_host = tk.Entry(window)
+entry_host.pack()
+
+label_start_port = tk.Label(window, text="Porta inicial:", font=("Arial", 12))
+label_start_port.pack()
+entry_start_port = tk.Entry(window)
+entry_start_port.pack()
+
+label_end_port = tk.Label(window, text="Porta final:", font=("Arial", 12))
+label_end_port.pack()
+entry_end_port = tk.Entry(window)
+entry_end_port.pack()
+
+button_scan = tk.Button(window, text="Começar verificação", command=scan_range)
+button_scan.pack()
+
+text_result = tk.Text(window)
+text_result.pack()
+
+status_label = tk.Label(window, text="", font=("Arial", 12))
+status_label.pack()
+
+window.mainloop()
